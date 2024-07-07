66bb350ce022b05cb63bc36aa1ca760cb2813339
BrentMRPalmer <bpalm015@uottawa.ca>
2024-07-07 08:51:19 -0400

    button routing

--- yarn/src/components/PatternCard.jsx
+++ yarn/src/components/PatternCard.jsx
@@ -1,24 +1,30 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Card } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function PatternCard({title, type, difficulty, sewing, length, yarn, size, image}) {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          <b>Plushy Type:</b> {type} <br />
-          <b>Difficulty:</b> {difficulty} <br />
-          <b>Sewing Level:</b> {sewing} <br />
-          <b>Length:</b> {length} <br />
-          <b>Yarn:</b> {yarn} <br />
-          <b>Size:</b> {size}
-        </Card.Text>
-        <Button variant="primary">View Pattern</Button>
-      </Card.Body>
-    </Card>
-  );
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/');
+    };
+
+    return (
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>
+            <b>Plushy Type:</b> {type} <br />
+            <b>Difficulty:</b> {difficulty} <br />
+            <b>Sewing Level:</b> {sewing} <br />
+            <b>Length:</b> {length} <br />
+            <b>Yarn:</b> {yarn} <br />
+            <b>Size:</b> {size}
+            </Card.Text>
+            <Button variant="primary" onClick={handleButtonClick}>View Pattern</Button>
+        </Card.Body>
+        </Card>
+    );
 }
 
 export default PatternCard;
