ecb12f67bd4067747783ed07dd8940a3ed0eb659
BrentMRPalmer <bpalm015@uottawa.ca>
2024-07-06 22:54:20 -0400

    Navbar

--- yarn/src/App.jsx
+++ yarn/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import NavbarC from './components/NavbarC';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Patterns from './pages/Patterns';
+import Guides from './pages/Guides';
+import Community from './pages/Community';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>HELLO</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <NavbarC />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/patterns" element={<Patterns />} />
+        <Route path="/guides" element={<Guides />} />
+        <Route path="/community" element={<Community />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
