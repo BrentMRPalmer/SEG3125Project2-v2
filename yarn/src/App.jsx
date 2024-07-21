fdad411b256d48c24947ad38f54dbe0fdd61c3cb
BrentMRPalmer <bpalm015@uottawa.ca>
2024-07-21 12:00:30 -0400

    plant

--- yarn/src/App.jsx
+++ yarn/src/App.jsx
@@ -65,7 +65,7 @@ const patternData = [
     id: 2,
     title: "Star Coin",
     type: "Object",
-    difficulty: "Easy",
+    difficulty: "Medium",
     sewing: "Medium",
     length: "1 hour",
     yarn: "Medium (4)",
@@ -94,6 +94,48 @@ const patternData = [
       "<b>R5:</b> R1: dc 3, dc inc* (50)",
       "Finish off. Leave an approx 36in. tail on the first coin. Then sew the two coins together.",
       "<b>Enjoy your new star coin!</b>"
+    ]
+  },
+  {
+    id: 3,
+    title: "Dangling Curly Plant",
+    type: "Plant",
+    difficulty: "Easy",
+    sewing: "Medium",
+    length: "2 hours",
+    yarn: "Bulky (5)",
+    size: "Medium", 
+    image: "/images/plant.png",
+    tools: [
+      "<b>Hook:</b> 5.5mm", 
+      "<b>Yarn:</b> Green and Brown",
+      "<b>Stuffing</b>",
+      "<b>Stitch Markers</b>",
+      "<b>Scissors</b>",
+      "<b>Sewing Needle</b>"
+    ],
+    instructions: [
+      "<b>Soil Pot:</b>",
+      "Start with brown yarn",
+      "<b>R1:</b> 6sc in a magic ring (6)",
+      "<b>R2:</b> 6inc (12)",
+      "<b>R3:</b> (sc, inc)x6 (18)",
+      "<b>R4:</b> (2sc, inc)x6 (24)",
+      "<b>R5-9:</b> 24sc (24 for 5 rounds)",
+      "<b>R10:</b> (4sc, dec)x4 (20)",
+      "<b>R11:</b> 20sc (20)",
+      "Slip stitch, fasten off and leave a long tail for sewing. Do not stuff yet.",
+      "<br />",
+      "<b>Curly Leaves:</b> (make 10 or as many as you want)",
+      "Start with green yarn",
+      "<b>R1:</b> Chain number for desired length and leave 4in tail for attaching to the soil",
+      "<b>R2:</b> 3sc in the 2nd chain fom your hook and 3sc in each chain after until the end of the chain",
+      "The curl will start to form, you may need to use your fingers to twist it into shape",
+      "Finish off with about a 4in tail for attaching to the soil",
+      "Repeat pattern for Curly Leaves with different starting lengths until you have your desired amount.",
+      "Sew all of the leaves onto the soil pot using both tail ends of each leaf and secure with a knot inside the pot",
+      "Stuf the soil pot, fasten off, and sew closed.",
+      "Display your new dangling curly plant!"
     ]
   },
 ]
